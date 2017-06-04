d0099624ad884efa7bcae06bb3d6c83df6dc8e1f
comatose <comatose>
2017-06-04 21:04:38 +0900

    Added an event system.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,12 @@
 rm -rf build ext
 mkdir build
 # cmake -H. -Bbuild -DCMAKE_CXX_COMPILER=clang++
-cmake -H. -Bbuild -DCMAKE_CXX_INCLUDE_WHAT_YOU_USE=iwyu -DCMAKE_CXX_COMPILER=clang++
+# cmake -H. -Bbuild -DCMAKE_CXX_INCLUDE_WHAT_YOU_USE=iwyu -DCMAKE_CXX_COMPILER=clang++
+cmake -H. -Bbuild -DCMAKE_CXX_COMPILER=clang++
 # cmake -H. -Bbuild -DCMAKE_CXX_INCLUDE_WHAT_YOU_USE=iwyu -DCMAKE_CXX_COMPILER=clang++ -GNinja
 # cmake -H. -Bbuild -DCMAKE_CXX_INCLUDE_WHAT_YOU_USE=iwyu -DCMAKE_CXX_CLANG_TIDY=clang-tidy
 
-# cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .
+# cmake -H. -Bbuild -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .
 
 # cmake --build build -- VERBOSE=1
 # cmake --build build
---
+++ event.cpp
@@ -0,0 +1,60 @@
+#define BOOST_HANA_CONFIG_ENABLE_STRING_UDL
+#include <boost/hana.hpp>
+
+#include <functional>
+#include <vector>
+#include <iostream>
+
+namespace hana = boost::hana;
+
+using Callback = std::function<void()>;
+
+template <class EventMap>
+struct event_system {
+  EventMap map_;
+
+  template <typename Event, typename F>
+  void on(Event e, F callback) {
+    auto is_known_event = hana::contains(map_, e);
+    static_assert(is_known_event,
+                  "trying to add a callback to an unknown event");
+
+    map_[e].push_back(callback);
+  }
+
+  template <typename Event>
+  void trigger(Event e) const {
+    auto is_known_event = hana::contains(map_, e);
+    static_assert(is_known_event,
+                  "trying to trigger an unknown event");
+
+    for (auto& callback : map_[e])
+      callback();
+  }
+};
+
+template <typename EventMap>
+event_system<EventMap> make_event_system_impl(EventMap em) {
+  return {em};
+}
+
+template <typename ...Events>
+auto  make_event_system(Events ...events) {
+  return make_event_system_impl(hana::make_map(hana::make_pair(events, std::vector<Callback>{})...));
+}
+
+int main() {
+  using namespace hana::literals;
+
+  auto events = make_event_system("foo"_s, "bar"_s, "baz"_s);
+
+  events.on("foo"_s, []() { std::cout << "foo triggered!" << '\n'; });
+  events.on("foo"_s, []() { std::cout << "foo again!" << '\n'; });
+  events.on("bar"_s, []() { std::cout << "bar triggered!" << '\n'; });
+  events.on("baz"_s, []() { std::cout << "baz triggered!" << '\n'; });
+  // events.on("unknown"_s, []() {}); // compiler error!
+
+  events.trigger("foo"_s); // no overhead
+  events.trigger("baz"_s);
+  // events.trigger("unknown"_s); // compiler error!
+}
